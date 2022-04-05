fb8baef35ace689e1244df34d7a5cae7488f83cf
healym3 <healym3@gator.uhd.edu>
2022-04-05 11:48:53 -0500

    Load bitmap from file manager

--- app/src/main/java/healym3/encryptionapp2/ui/aes/AesFragment.java
+++ app/src/main/java/healym3/encryptionapp2/ui/aes/AesFragment.java
@@ -1,19 +1,26 @@
 package healym3.encryptionapp2.ui.aes;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.bumptech.glide.Glide;
 
 import healym3.encryptionapp2.databinding.FragmentAesBinding;
 
 public class AesFragment extends Fragment {
 
+    private final int CHOOSE_BMP_FROM_DEVICE = 1000;
     private FragmentAesBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -24,8 +31,11 @@ public class AesFragment extends Fragment {
         binding = FragmentAesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        AESViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        binding.buttonLoadBitmap.setOnClickListener(view -> {
+            chooseBmpFromDevice();
+        });
+        //final TextView textView = binding.textNotifications;
+        //AESViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
@@ -34,4 +44,31 @@ public class AesFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    private void chooseBmpFromDevice(){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_BMP_FROM_DEVICE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CHOOSE_BMP_FROM_DEVICE && resultCode == Activity.RESULT_OK){
+
+            if(data != null){
+                Uri imageUriOriginal = data.getData();
+                Log.d("TAG", "onActivityResult: " + imageUriOriginal.getPath());
+                //textView.setText(imageUriOriginal.getEncodedPath());
+                Glide.with(getContext())
+                        .load(imageUriOriginal)
+                        .fitCenter()
+                        .into(binding.imageViewOriginal);
+                //imageUri.
+                //bitmap
+            }
+        }
+    }
+
 }
