e49c25f9f2740e60f219728a4194b0fdb2fbb5d1
healym3 <healym3@gator.uhd.edu>
2022-04-06 12:30:23 -0500

    Cleaned up BreakerFragment issues and Added icon for Breaker to Navigation.

--- app/src/main/java/healym3/encryptionapp2/ui/breaker/BreakerFragment.java
+++ app/src/main/java/healym3/encryptionapp2/ui/breaker/BreakerFragment.java
@@ -1,7 +1,7 @@
 package healym3.encryptionapp2.ui.breaker;
 
 import android.app.Activity;
-import android.content.ContentResolver;
+
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -16,20 +16,14 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Objects;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
+
 
 import healym3.encryptionapp2.algorithms.Breaker;
-import healym3.encryptionapp2.data.UserFile;
+
 import healym3.encryptionapp2.databinding.FragmentBreakerBinding;
 import healym3.encryptionapp2.util.Utils;
 
@@ -46,40 +40,20 @@ public class BreakerFragment extends Fragment {
 
         binding = FragmentBreakerBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        binding.cipherToBreakEditText.setText("msr crkqimurem xz jxukdmri bjvrejr qec renverriven mrjsextxna vb xer " +
-                "xz msirr qjqcruvj crkqimuremb pvmsve msr jxttrnr xz bjvrejrb qec " +
-                "mrjsextxna qm msr devoribvma xz sxdbmxe cxpemxpe. pr xzzri bmdcremb q " +
-                "devwdr xkkximdevma mx qjsvror qjqcruvj bdjjrbb ve jxukdmri bjvrejr qec " +
-                "renverriven mrjsextxna msixdns ivnxixdb qec ryjvmven jxdibrb, " +
-                "vejtdcven decriniqcdqmr irbrqijs xkkximdevmvrb. pr qir jxuuvmmrc mx " +
-                "miqveven bmdcremb zxi jsqttrenrb ve jxukdmri bjvrejr qec renverriven " +
-                "mrjsextxna, qec ve zvrtcb irwdviven msr gexptrcnr qec bgvttb, xdi " +
-                "jdiivjdtdu kixovcrb zxi bmdcremb pvmsve mryqb qec qixdec msr pxitc. " +
-                "msr crkqimurem xz jxukdmri bjvrejrb qec renverriven mrjsextxna vb msr " +
-                "zqbmrbm nixpven crkqimurem pvmsve msr jxttrnr xz bjvrejrb qec " +
-                "mrjsextxna. qccvmvxeqtta, qb q bmdcrem xz msr jbrm crkqimurem, axd " +
-                "pvtt nqve sqecb xe rykrivrejr qec kiqjmvjqt miqveven. bmdcremb pvtt fr " +
-                "irwdvirc mx kqimvjvkqmr ve q brurbmri kixhrjm, psvjs pvtt miqve " +
-                "bmdcremb mx pxig xe irqt vecdbmia kixftrub ve q mrqu reovixeurem msqm " +
-                "msra pvtt rejxdemri pxigven ve vecdbmia qzmri niqcdqmvxe. msr brevxi " +
-                "kixhrjm qttxpb bmdcremb mx qkkta msrvi jxdibr uqmrivqtb qec trqieven " +
-                "rykrivrejrb mx brurbmri txen irqt pxitc kixhrjmb.");
+
 
         binding.breakCipherButton.setOnClickListener(view -> {
             if(!TextUtils.isEmpty(binding.cipherToBreakEditText.getText())){
                 Breaker breaker = new Breaker(getContext());
                 breaker.breakCipher(binding.cipherToBreakEditText.getText().toString());
                 binding.cipherBreakerResultEditText.setText(breaker.getBreakerResult());
-                Utils.hideSoftKeyboard(getContext(),view);
+                Utils.hideSoftKeyboard(requireContext(),view);
             }
 
         });
 
-        binding.openCipherTextButton.setOnClickListener(view -> {
-            chooseTextFile();
-        });
-        //final TextView textView = binding.textDashboard;
-        //breakerViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        binding.openCipherTextButton.setOnClickListener(view -> chooseTextFile());
+
         return root;
     }
 
@@ -111,7 +85,7 @@ public class BreakerFragment extends Fragment {
             StringBuilder stringBuilder = new StringBuilder();
             String line;
             while((line = bufferedReader.readLine()) != null){
-                stringBuilder.append(line + "\n");
+                stringBuilder.append(String.format("%s\n", line));
             }
             setCipherText(stringBuilder.toString());
             bufferedReader.close();
